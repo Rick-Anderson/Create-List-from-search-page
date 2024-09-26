6325c7af2824f90e6bffb8d75c85a0bdf8cd9f2f
Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>
2024-09-25 16:27:21 -1000

    read urlSegment

--- Program.cs
+++ Program.cs
@@ -1,41 +1,51 @@
 ﻿using System.Text;
 
-string input = File.ReadAllText("..\\..\\..\\Search.txt");
-
-string[] lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-StringBuilder inputCleaned = new StringBuilder();
-
-// remove all lines except the title and /aspnet/ which is used to create URL.
-for (int i = 0; i < lines.Length; i++)
+internal class Program
 {
-    if (lines[i].Contains("/aspnet/"))
+    static void Main(string[] args)
     {
-        if (i > 0)
+        var urlSegement = "/aspnet/";
+
+        if (args.Length > 0) { urlSegement = args[0]; }
+
+        string input = File.ReadAllText("..\\..\\..\\Search.txt");
+
+        string[] lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        StringBuilder inputCleaned = new StringBuilder();
+
+        // remove all lines except the title and /aspnet/ which is used to create URL.
+        for (int i = 0; i < lines.Length; i++)
         {
-            inputCleaned.AppendLine(lines[i - 1]);
+            if (lines[i].Contains(urlSegement))
+            {
+                if (i > 0)
+                {
+                    inputCleaned.AppendLine(lines[i - 1]);
+                }
+                inputCleaned.AppendLine(lines[i]);
+            }
         }
-        inputCleaned.AppendLine(lines[i]);
-    }
-}
 
-//File.WriteAllText("..\\..\\..\\SearchCleaned.txt", inputCleaned.ToString());
+        //File.WriteAllText("..\\..\\..\\SearchCleaned.txt", inputCleaned.ToString());
 
-// Create GitHub markup, a checkbox and the title as a link to the URL.
-string[] cleanedLines = inputCleaned.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+        // Create GitHub markup, a checkbox and the title as a link to the URL.
+        string[] cleanedLines = inputCleaned.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.None);
 
-using (StreamWriter writer = new StreamWriter("..\\..\\..\\formatted.txt"))
-{
-    for (int i = 0; i < cleanedLines.Length; i += 2)
-    {
-        if (i + 1 < cleanedLines.Length)
+        using (StreamWriter writer = new StreamWriter("..\\..\\..\\formatted.txt"))
         {
-            string title = cleanedLines[i].Trim();
-            string urlPath = cleanedLines[i + 1].Trim();
-            string fullUrl = "https://docs.microsoft.com/en-us" + urlPath;
+            for (int i = 0; i < cleanedLines.Length; i += 2)
+            {
+                if (i + 1 < cleanedLines.Length)
+                {
+                    string title = cleanedLines[i].Trim();
+                    string urlPath = cleanedLines[i + 1].Trim();
+                    string fullUrl = "https://docs.microsoft.com/en-us" + urlPath;
 
-            string formattedOutput = ($"- [ ] [{title}]({fullUrl})");
-            Console.WriteLine(formattedOutput);
-            writer.WriteLine(formattedOutput);
+                    string formattedOutput = ($"- [ ] [{title}]({fullUrl})");
+                    Console.WriteLine(formattedOutput);
+                    writer.WriteLine(formattedOutput);
+                }
+            }
         }
     }
 }
